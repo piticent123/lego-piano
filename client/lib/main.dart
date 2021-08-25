dd27f8ee517096dc3076df980afcafb72f4245b6
Jordan Pitlor <jordan.pitlor@avasure.com>
2021-08-25 11:44:04 -0400

    bottom nav

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -6,32 +6,40 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<Widget> pages = [ListenToMusic(), PlayMusic()];
+  int _currentIndex = 0;
+
+  void handleTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'LEGO Piano Companion',
-      theme: ThemeData(primarySwatch: Colors.grey),
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("LEGO Piano Companion"),
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.hearing)),
-                Tab(icon: Icon(Icons.touch_app)),
+        title: 'LEGO Piano Companion',
+        theme: ThemeData(primarySwatch: Colors.grey),
+        home: DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            appBar: AppBar(title: Text("LEGO Piano Companion")),
+            body: pages[_currentIndex],
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: _currentIndex,
+              onTap: handleTabTapped,
+              items: [
+                BottomNavigationBarItem(icon: Icon(Icons.hearing), label: "Listen"),
+                BottomNavigationBarItem(icon: Icon(Icons.touch_app), label: "Play"),
               ],
             ),
           ),
-          body: TabBarView(
-            children: [
-              ListenToMusic(),
-              PlayMusic()
-            ],
-          )
-        ),
-      )
-    );
+        ));
   }
 }
