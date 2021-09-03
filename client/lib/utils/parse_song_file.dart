1816831cff15101022354d10a21e10833acc0601
Jordan Pitlor <jordan.pitlor@avasure.com>
2021-09-03 08:36:55 -0400

    add key to song

--- client/lib/utils/parse_song_file.dart
+++ client/lib/utils/parse_song_file.dart
@@ -9,26 +9,34 @@ class Note {
 }
 
 class Song {
+  final Scale key;
   final int tempo;
   final List<List<Note>> bassClef;
   final List<List<Note>> trebleClef;
 
-  Song(this.tempo, this.bassClef, this.trebleClef);
+  Song(this.key, this.tempo, this.bassClef, this.trebleClef);
 }
 
 final _restPitchPlaceholder = Pitch.parse("c4");
 Song parse(String csv) {
   final content = csv.split("\n");
   final header = content.removeAt(0);
-  final match = RegExp(r"^(\d+),(\d+),(\d+)$").firstMatch(header);
+  final match = RegExp(r"^(.*?),(.*?),(\d+),(\d+),(\d+)$").firstMatch(header);
 
-  if (match == null || match.groupCount != 4) {
-    throw Exception("Song header malformed - it must be a comma separated (no spaces) list of 3 numbers: tempo, number of parts on treble clef, and number of parts on bass clef.");
+  if (match == null || match.groupCount != 6) {
+    throw Exception("Song header malformed - it must look like `a,b,c,d,e`\n"
+        "\t`a` is the tonic pitch\n"
+        "\t`b` is the mode of the key\n"
+        "\t`c` is the tempo of the song\n"
+        "\t`d` is the number of parts on the treble clef\n"
+        "\t`e` is the number of parts on the bass clef");
   }
 
-  final tempo = int.parse(match.group(1)!);
-  final trebleClef = List.generate(int.parse(match.group(2)!), (index) => List<Note>.empty(growable: true));
-  final bassClef = List.generate(int.parse(match.group(3)!), (index) => List<Note>.empty(growable: true));
+  final tonic = Pitch.parse(match.group(1)!).pitchClass;
+  final key = ScalePattern.findByName(match.group(2)!).at(tonic);
+  final tempo = int.parse(match.group(3)!);
+  final trebleClef = List.generate(int.parse(match.group(4)!), (index) => List<Note>.empty(growable: true));
+  final bassClef = List.generate(int.parse(match.group(5)!), (index) => List<Note>.empty(growable: true));
   for (var line in content) {
     final notes = line.split(",");
     if (notes.length != trebleClef.length + bassClef.length) {
@@ -51,5 +59,5 @@ Song parse(String csv) {
     }
   }
 
-  return Song(tempo, bassClef, trebleClef);
+  return Song(key, tempo, bassClef, trebleClef);
 }
